7b54c288b2f3c2d74fc300c5344dcf1e8f450851
trikitrok <trikitrok@gmail.com>
2015-07-21 14:37:14 +0200

    Refactor: improved a test description

--- binary_search_tree_spec.rb
+++ binary_search_tree_spec.rb
@@ -1,7 +1,7 @@
 require './binary_search_tree'
 
 describe BinarySearchTree do
-  it "creates a tree with just one node" do
+  it "creates a tree without any sons" do
     tree = BinarySearchTree.new(3)
 
     expect(tree.value()).to equal 3
