606cfc67a04e82da01e304130c6f4fa5eac595d7
trikitrok <trikitrok@gmail.com>
2015-07-21 14:36:23 +0200

    Refactor: substituted two tests by creating a new more complete one

--- binary_search_tree_spec.rb
+++ binary_search_tree_spec.rb
@@ -7,19 +7,13 @@ describe BinarySearchTree do
     expect(tree.value()).to equal 3
   end
 
-  it "creates a tree with a node that has a left son" do
-    tree = BinarySearchTree.new(4)
-    tree.insert(1)
-
-    expect(tree.value()).to equal 4
-    expect(tree.left().value()).to equal 1
-  end
-
-  it "creates a tree with a node that has a right son" do
+  it "creates a tree with left and right sons" do
     tree = BinarySearchTree.new(2)
     tree.insert(5)
+    tree.insert(1)
 
     expect(tree.value()).to equal 2
+    expect(tree.left().value()).to equal 1
     expect(tree.right().value()).to equal 5
   end
 
