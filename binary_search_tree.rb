5d4dcaf96555573029b198197982c092617e4b50
trikitrok <trikitrok@gmail.com>
2015-07-21 13:51:30 +0200

    It creates a tree with one node that has a right son

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,14 +1,18 @@
 class BinarySearchTree
-  attr_reader :value, :left
+  attr_reader :value, :left, :right
 
   def initialize(value)
     @value = value
   end
 
   def insert(value)
-    self.left = BinarySearchTree.new(value)
+    if value < self.value
+      self.left = BinarySearchTree.new(value)
+    else
+      self.right = BinarySearchTree.new(value)
+    end
   end
 
   private
-  attr_writer :left
+  attr_writer :left, :right
 end
--- binary_search_tree_spec.rb
+++ binary_search_tree_spec.rb
@@ -14,4 +14,12 @@ describe BinarySearchTree do
     expect(tree.value()).to equal 4
     expect(tree.left().value()).to equal 1
   end
+
+  it "creates a tree with a node that has a right son" do
+    tree = BinarySearchTree.new(2)
+    tree.insert(5)
+
+    expect(tree.value()).to equal 2
+    expect(tree.right().value()).to equal 5
+  end
 end
